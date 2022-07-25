ec555f172cd6b034d96627a3fb4936f62832aa75
Rahul1999Choubey <choubeyrahul1999@gmail.com>
2022-07-25 11:07:32 +0530

    Initial Commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+import Editor from './Component/Editor';
+
+function App() {
+  return (
+    <>
+      <Editor/>
+     
+    </>
+  );
+}
+
+export default App;
